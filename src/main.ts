d62ee9e7967deadc045471c69c191ecaec7b6b79
Eitan Biletski <87935299+shaijyy@users.noreply.github.com>
2025-03-07 22:25:58 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ function noSearchDefaultPageRender() {
         •
         <a href="https://github.com/t3dotgg/unduck" target="_blank">source code</a>
         •
-        <a href="#defaultbang" target="_blank">change default !bang</a>
+        <a href="#" onClick="bangSelect()">change default !bang</a>
       </footer>
     </div>
   `;
