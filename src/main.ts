a47563704edbd5d5d5ec82141ba911345eaa4550
Eitan Biletski <87935299+shaijyy@users.noreply.github.com>
2025-03-07 22:05:00 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -25,11 +25,9 @@ function noSearchDefaultPageRender() {
         </div>
       </div>
       <footer class="footer">
-        <a href="https://t3.chat" target="_blank">t3.chat</a>
-        •
         <a href="https://x.com/theo" target="_blank">theo</a>
         •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
+        <a href="https://github.com/t3dotgg/unduck" target="_blank">source code</a>
         •
         <a href="mailto:unduckhelp@duck.com" target="_blank">suggest a !bang</a>
       </footer>
